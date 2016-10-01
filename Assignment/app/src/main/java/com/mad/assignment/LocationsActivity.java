b5a284f100a5530bc5496d7eea85432fafad2565
platinumgamer <platinumgamer@hotmail.com>
2016-10-01 16:53:03 +1000

    implemented locations list

--- Assignment/app/src/main/java/com/mad/assignment/LocationsActivity.java
+++ Assignment/app/src/main/java/com/mad/assignment/LocationsActivity.java
@@ -1,14 +1,23 @@
 package com.mad.assignment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class LocationsActivity extends AppCompatActivity {
+
+    public static final String LOCATION_PREF = "locationPref";
+
+    ListView mListView;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,34 @@ public class LocationsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mListView = (ListView) findViewById(R.id.locations_activity_list_view);
+
+        String[] addresses = getAddressesFromPrefs();
+
+        mAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, addresses);
+
+        mListView.setAdapter(mAdapter);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String[] addresses = getAddressesFromPrefs();
+
+        mAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, addresses);
+
+        mListView.setAdapter(mAdapter);
+        Log.d("tag", "resumed");
+    }
+
+    private String[] getAddressesFromPrefs() {
+        SharedPreferences sharedPreferences = getSharedPreferences(LOCATION_PREF, Context.MODE_PRIVATE);
+
+        String addressesAsString = sharedPreferences.getString("myList", "Not found");
+        return addressesAsString.split(",");
+    }
 }
--- Assignment/app/src/main/java/com/mad/assignment/MapsActivity.java
+++ Assignment/app/src/main/java/com/mad/assignment/MapsActivity.java
@@ -27,26 +27,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.Manifest;
 import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.Geofence;
@@ -55,18 +43,12 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements
@@ -126,6 +108,39 @@ public class MapsActivity extends FragmentActivity implements
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
                     }
                 }
+            }
+        });
+
+        Button saveBtn = (Button) findViewById(R.id.maps_activity_save_btn);
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = searchBar.getText().toString();
+                SharedPreferences sharedPreferences = getSharedPreferences(LocationsActivity.LOCATION_PREF, Context.MODE_PRIVATE);
+
+                String addressesAsString = sharedPreferences.getString("myList", "Not found");
+                String[] addresses = addressesAsString.split(",");
+
+                List<String> listOfAddresses = new ArrayList<String>();
+
+                for(int i=0; i < addresses.length; i++){
+                    listOfAddresses.add(addresses[i]);
+                }
+
+                listOfAddresses.add(location);
+
+                StringBuilder listBuilder = new StringBuilder();
+                for(String s : listOfAddresses){
+                    listBuilder.append(s);
+                    listBuilder.append(",");
+                }
+
+                SharedPreferences.Editor editor = getSharedPreferences(LocationsActivity.LOCATION_PREF, MODE_PRIVATE).edit();
+                editor.putString("myList", listBuilder.toString());
+                editor.commit();
+
+                Log.v(TAG, sharedPreferences.getString("myList", "Not found"));
             }
         });
     }
