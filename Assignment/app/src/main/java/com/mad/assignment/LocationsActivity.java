ed9a83123db8c82f29fa9a5379b3c07873eddaf3
platinumgamer <platinumgamer@hotmail.com>
2016-10-01 15:51:53 +1000

    added new locations activity

--- Assignment/app/src/main/java/com/mad/assignment/LocationsActivity.java
+++ Assignment/app/src/main/java/com/mad/assignment/LocationsActivity.java
@@ -7,14 +7,13 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class LocationsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_locations);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,16 +21,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        Button setLocationsBtn = (Button) findViewById(R.id.main_activity_set_locations_btn);
-        setLocationsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LocationsActivity.class);
+                Intent intent = new Intent(LocationsActivity.this, MapsActivity.class);
                 startActivity(intent);
             }
         });
