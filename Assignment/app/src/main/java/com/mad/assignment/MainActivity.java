bc01188d8e7e171734b08bc9d25b23f829454e92
platinumgamer <platinumgamer@hotmail.com>
2016-10-01 11:32:09 +1000

    Initial commit

---
+++ Assignment/app/src/main/java/com/mad/assignment/MainActivity.java
@@ -0,0 +1,40 @@
+package com.mad.assignment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button setLocationsBtn = (Button) findViewById(R.id.main_activity_set_locations_btn);
+        setLocationsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ Assignment/compile into jar.bat
@@ -0,0 +1,3 @@
+@echo off
+jar cvf ../Assignment.jar app/src/main app/build/outputs/apk/app-debug.apk
+pause
