49fd259b1fdba209129fd6dccd08a427c9a8dc65
platinumgamer <platinumgamer@hotmail.com>
2016-10-06 15:59:44 +1100

    added bg location service

--- Assignment/app/src/main/java/com/mad/assignment/MainActivity.java
+++ Assignment/app/src/main/java/com/mad/assignment/MainActivity.java
@@ -35,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        startService(new Intent(this, LocationTracker.class));
     }
 
 }
